2e1f6ec383442045e219308bf60996b5ebafb492
Marlon Andrade <marlonmandrade@gmail.com>
2013-04-21 15:02:43 -0300

    corrigindo animacao do shine layer

--- guess_it_bsi/GuessItBSI/view/GIShineLabel.m
+++ guess_it_bsi/GuessItBSI/view/GIShineLabel.m
@@ -10,6 +10,13 @@
 
 #import <QuartzCore/QuartzCore.h>
 
+@interface GIShineLabel ()
+
+@property (nonatomic, strong) UILabel *shineLabel;
+@property (nonatomic, strong) CALayer *maskLayer;
+
+@end
+
 @implementation GIShineLabel
 
 #pragma mark - Getter
@@ -21,36 +28,55 @@
     return _shineColor;
 }
 
+- (UILabel *)shineLabel {
+    if (!_shineLabel) {
+        _shineLabel = [UILabel label];
+        _shineLabel.font = self.font;
+        _shineLabel.textAlignment = self.textAlignment;
+        _shineLabel.backgroundColor = [UIColor clearColor];
+        _shineLabel.layer.mask = self.maskLayer;
+    }
+    return _shineLabel;
+}
+
+- (CALayer *)maskLayer {
+    if (!_maskLayer) {
+        _maskLayer = [CALayer layer];
+        _maskLayer.backgroundColor = [UIColor clearColor].CGColor;
+        _maskLayer.contents = (id)[UIImage imageNamed:@"shine_mask"].CGImage;
+        _maskLayer.contentsGravity = kCAGravityCenter;
+    }
+    return _maskLayer;
+}
+
 #pragma mark - Public Interface
 
 - (void)flash {
     CGFloat width = self.bounds.size.width;
     CGFloat height = self.bounds.size.height;
 
-    UILabel *shineLabel = [UILabel labelWithFrame:CGRectMake(0.f, 1.f, self.bounds.size.width, self.bounds.size.height)];
-    shineLabel.font = self.font;
-    shineLabel.text = self.text;
-    shineLabel.textColor = self.shineColor;
-    shineLabel.backgroundColor = [UIColor clearColor];
-    shineLabel.textAlignment = NSTextAlignmentCenter;
+    self.shineLabel.frame = CGRectMake(0.f, 1.f, width, height);
+    self.shineLabel.text = self.text;
+    self.shineLabel.textColor = self.shineColor;
 
-    CALayer *mask = [CALayer layer];
-    mask.backgroundColor = [UIColor clearColor].CGColor;
-    mask.contents = (id) [UIImage imageNamed:@"shine_mask"].CGImage;
-    mask.contentsGravity = kCAGravityCenter;
-    mask.frame = CGRectMake(-width, 0, width * 1.25f, height);
+    self.maskLayer.frame = CGRectMake(-width, 0, width * 1.25f, height);
 
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.x"];
     animation.byValue = @(width * 2.f);
-    animation.repeatCount = HUGE_VALF;
     animation.duration = 3.f;
     animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    animation.delegate = self;
 
-    CALayer *shineLayer = shineLabel.layer;
-    [self.layer addSublayer:shineLayer];
-    shineLayer.mask = mask;
+    [self.layer addSublayer:self.shineLabel.layer];
 
-    [mask addAnimation:animation forKey:@"shine"];
+    [self.maskLayer addAnimation:animation forKey:@"shine"];
+}
+
+#pragma mark - CAAnimation Delegate
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    [self.maskLayer removeAllAnimations];
+    [self.shineLabel.layer removeFromSuperlayer];
 }
 
 @end
