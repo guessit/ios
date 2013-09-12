75c9fbcf451f9fb32d400f6324c9f4b279465993
Marlon Andrade <marlonmandrade@gmail.com>
2013-09-12 07:50:15 -0300

    removendo chamadas a nslog e ajustando product id

--- GuessIt/categories/UINavigationController+GuessIt.m
+++ GuessIt/categories/UINavigationController+GuessIt.m
@@ -17,17 +17,12 @@
 @implementation UINavigationController (GuessIt)
 
 + (instancetype)guessItGame {
-    NSLog(@"Bundle: %@", [NSBundle mainBundle]);
-
     NSString *bundleId = [NSBundle mainBundle].bundleIdentifier.lowercaseString;
     NSMutableSet *identifiers = [NSMutableSet set];
     for (NSString *product in GI_IAP) {
-//        NSString *productId = [NSString stringWithFormat:@"%@.%@", bundleId, product];
-        NSString *productId = product;
+        NSString *productId = [NSString stringWithFormat:@"%@.%@", bundleId, product];
         [identifiers addObject:productId];
     }
-
-    NSLog(@"Identifiers: %@", identifiers);
 
     [[CargoBay sharedManager] productsWithIdentifiers:identifiers
                                               success:^(NSArray *products, NSArray *invalidIdentifiers) {
